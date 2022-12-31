2919ea45d48c84709200ee31190e0dc51656fcea
Tushar Joshi <tusharjoshi2015@gmail.com>
2022-12-31 17:36:25 +0530

    second

---
+++ main.js
@@ -0,0 +1,89 @@
+
+// if (true) {
+//     let a = 2
+//     // var a = 2;
+//   }
+//   console.log(a)
+
+
+//   let a = 42
+//   {
+//     let a = 100
+//   }
+//   console.log(a)
+
+
+//   let a = 100
+// function App() {
+//   console.log('1', a) - due to TDZ
+//   let a = 42
+//   console.log('2', a)
+//   {
+//     let a = 100
+//   }
+//   console.log('3', a)
+// }
+
+// App();
+
+
+// let a = 100;
+// function App() {
+//     a = 42;
+//     console.log("1", a);
+// }
+
+// App();
+
+// function abs(){
+//     console.log(a); - due to TDZ
+//     let a = 2;
+// }
+
+// abs()
+
+
+// function abs(){
+//     console.log(a);
+//     var a = 3
+// }
+
+// abs()
+
+// const tanay = 2
+// tanay = 3
+
+
+// const obj = { a: 1, b: 2}
+// obj.a = 2;
+// obj.c = 4;
+
+// console.log(obj)
+
+// obj = {a: 5};
+// console.log(obj)
+
+
+
+// const obj = {a: 3, b: 6}
+// const obj2 = {a:2, b:4}
+
+// obj = obj2
+
+
+const array = [1, 2, 3, 4]
+array.push(66);
+
+console.log(array)
+
+
+const array2 = [1, 2, 3, 4]
+array2 = array2.push(55);
+
+
+
+
+
+
+ 
+
